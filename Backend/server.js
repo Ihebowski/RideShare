5d150fb49e675db81aeea1ec020d7b2906b61f8f
Ihebowski <iheb.barrah@gmail.com>
2024-10-21 16:37:00 +0100

    Changes in the Db name

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/carpool', {
+mongoose.connect('mongodb://localhost:27017/rideshare', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
