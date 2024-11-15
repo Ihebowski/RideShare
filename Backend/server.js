a7a5ec2b97b09307ee5ac67e1ad57b2b802fa93b
Ihebowski <iheb.barrah@gmail.com>
2024-11-15 15:12:43 +0100

    Changes in package.json

--- Backend/server.js
+++ Backend/server.js
@@ -58,3 +58,5 @@ const PORT = process.env.PORT || 3000; // Use environment variable for port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
